7a18249f38a66046b9b9aa27b765f253e742a158
manojkunapalli <manojkunapalli@gmail.com>
2020-09-29 19:14:47 +0530

    Modified: run the loop till they reach 21 times

--- stimulateFlipCoin.sh
+++ stimulateFlipCoin.sh
@@ -4,23 +4,33 @@ echo "Welcome to flip coin stimulating problem"
 echo "fliping a coin!!"
 echo -e
 
-read -p "number of coin flips = " number
 headCount=0;
-tailCount=0
+tailCount=0;
 
-for i in `seq $number`
+while [ $headCount -le 20 ] || [ $tailCount -le 20 ]
 do
-	a=$(( 1 + RANDOM % 2 ))
+	a=$((1+RANDOM%2))
 	if [ $a -eq 1 ]
 	then
-		((headCount++))
+		echo "Heads"
+		headCount=$(($headCount + 1))
 	elif [ $a -eq 2 ]
 	then
-		((tailCount++))
+		echo "Tails"
+		tailCount=$(($tailCount + 1))
 	fi
 done
 
 echo "number of times head has won = $headCount"
 echo "number of times tail has won = $tailCount"
 
+if [ $headCount -eq 21 -a $tailCount ]
+then
+	echo "It is a tie!!!"
+elif [ $headCount -eq 21 ]
+then
+	echo "<----Heads wins the game---->"
+else
+	echo "<----Tails wins the game---->"
+fi
 
