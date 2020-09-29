0fce8d33eed5e462f15c5e17485a43aab030c135
manojkunapalli <manojkunapalli@gmail.com>
2020-09-29 16:21:03 +0530

    added: stimulate multiple flip coin

--- stimulateFlipCoin.sh
+++ stimulateFlipCoin.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 echo "Welcome to flip coin stimulating problem"
 
-a=$(( 1 + RANDOM % 2 ))
-
 echo "fliping a coin!!"
+echo -e
 
-if [ $a -eq 1 ]
-then
-	echo "Heads Won"
-elif [ $a -eq 2 ]
-then
-	echo "Tails Won"
-fi
+read -p "number of coin flips = " number
+headCount=0;
+tailCount=0
+
+for i in `seq $number`
+do
+	a=$(( 1 + RANDOM % 2 ))
+	if [ $a -eq 1 ]
+	then
+		((headCount++))
+	elif [ $a -eq 2 ]
+	then
+		((tailCount++))
+	fi
+done
+
+echo "number of times head has won = $headCount"
+echo "number of times tail has won = $tailCount"
 
 
