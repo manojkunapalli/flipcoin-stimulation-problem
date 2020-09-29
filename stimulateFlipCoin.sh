8c5117e64bcd7b18769b5c23b65c5605a2aa4add
manojkunapalli <manojkunapalli@gmail.com>
2020-09-29 23:20:31 +0530

    Changed: Flip coin untill both are equal & until it reaches difference 2

--- stimulateFlipCoin.sh
+++ stimulateFlipCoin.sh
@@ -5,9 +5,9 @@ echo "fliping a coin!!"
 echo -e
 
 headCount=0;
-tailCount=0;
+tailCount=1;
 
-while [ $headCount -lt 21 ] && [ $tailCount -lt 21 ]
+while [ $headCount -ne $tailCount ]
 do
 	a=$(( 1 + RANDOM % 2 ))
 	if [ $a -eq 1 ]
@@ -21,13 +21,26 @@ done
 echo "number of times head has won = $headCount"
 echo "number of times tail has won = $tailCount"
 
-if [ $headCount -eq 21 -a $tailCount -eq 21 ]
+if [ $headCount -eq $tailCount ]
 then
 	echo "It is a tie!!!"
-elif [ $headCount -eq 21 ]
-then
-	echo "<----Heads wins the game---->"
-else
-	echo "<----Tails wins the game---->"
+	while [ $(($headCount-$tailCount)) -ne 2 ]
+	do
+		f=$(( 1 + RANDOM % 2 ))
+		if [ $f -eq 1 ]
+		then
+			headCount=`expr $headCount + 1`
+		else
+			tailCount=`expr $tailCount + 1`
+		fi
+	done
 fi
 
+if [ $headCount > $tailCount ]
+then
+	echo "Heads wins the game"
+	echo "Heads = $headCount"
+else
+	echo "Tails wins the game"
+	echo "Tails = $tailCount"
+fi
