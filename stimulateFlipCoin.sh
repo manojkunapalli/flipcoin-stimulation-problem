ade93695afcaec410ab6e9b025771d8efff16fcd
manojkunapalli <manojkunapalli@gmail.com>
2020-09-29 15:55:16 +0530

    added: flipping coin and display Heads or winner

--- stimulateFlipCoin.sh
+++ stimulateFlipCoin.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 echo "Welcome to flip coin stimulating problem"
 
+a=$(( 1 + RANDOM % 2 ))
+
+echo "fliping a coin!!"
+
+if [ $a -eq 1 ]
+then
+	echo "Heads Won"
+elif [ $a -eq 2 ]
+then
+	echo "Tails Won"
+fi
+
+
